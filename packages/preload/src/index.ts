666df06ec192052de3974a3e59bfb9407d1a3d76
JoseMoreville <18040006@utculiacan.edu.mx>
2022-04-12 21:00:39 -0600

    Initial commit

---
+++ packages/preload/src/index.ts
@@ -0,0 +1,6 @@
+/**
+ * @module preload
+ */
+
+import './nodeCrypto';
+import './versions';
---
+++ packages/renderer/src/App.vue
@@ -0,0 +1,22 @@
+<script lang="ts" setup>
+/*
+Obten el nombre del fondo seleccionado y  luego asignalo a la propiedad background de la clase App mediante una llamada al ipc de electron
+Si el usuario cambia de fondo entonces el main de electron debe actualizar el fondo de la aplicacion
+si el usuario sube se agrega un nuevo fondo a la lista de fondos
+la mayoria de los datos como favoritos, fondo seleccionado se guardan en el electron store
+*/
+
+</script>
+
+<template>
+  <img
+    src="../assets/logo.svg"
+  >
+</template>
+
+<style>
+  img {
+    width: 100%;
+    height: 100%;
+  }
+</style>
